af3ae1946958d2dadac99ee9ad5bf608c26e7dbf
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-05-18 22:33:12 +0530

    length, indexof, getslice

---
+++ JavaScript/L3.js
@@ -0,0 +1,14 @@
+// const str = "BlueDragon";
+// console.log(str.length)
+
+// function findIndexOf(str, target) {
+//     console.log("Original String:", str);
+//     console.log("Index:", str.lastIndexOf(target));
+// }
+// findIndexOf("Hello Bheem Bheem Bheem Chota Bheem", "Bheem");
+
+function getSlice(str, start, end) {
+    console.log("original String:", str);
+    console.log("After slice:", str.slice(start, end));
+}
+getSlice("Broooooooo what's up", 0, 3);
