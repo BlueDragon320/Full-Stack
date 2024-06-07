26d141fa44a6c1ec4bcb0b5b0d25fd66221161bd
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-06-07 21:10:01 +0530

    Add files via upload

---
+++ JavaScript/L3again.js
@@ -0,0 +1,97 @@
+// const str='pandya';
+// console.log(str.length);
+
+// function findIndexOf(str, target) {
+//     console.log("Original String:", str);
+//     console.log("Index:", str.indexOf(target));
+//   }
+// findIndexOf("Hello World", "World");
+
+
+// const str="pandu bhai";
+// let ans2= str.substr(2,4);
+// let ans= str.slice(2,4);
+// console.log(ans);
+// console.log(ans2);
+
+
+// const str="hello world";
+// console.log(str.replace("world", "Walter White"));
+
+// const value="hi, i'm pandu"
+// const words=value.split(",");
+// console.log(words);
+
+
+// const value="   pandu bhai   "
+// console.log(value.trim())
+
+// const value="  pandu Bhai   "
+// console.log(value.toLocaleUpperCase())
+
+
+
+// console.log(parseInt("42"));
+// console.log(parseInt("42px"));
+// console.log(parseInt("3.14"));
+
+// console.log(parseFloat("42"));
+// console.log(parseFloat("42px"));
+// console.log(parseFloat("3.14"));
+
+// const initialArray=[1,2,3];
+// const secondArray=[4,5,6];
+// console.log(initialArray.concat(secondArray));
+
+// function log1(){
+//     console.log("hello world 1")
+// }
+// function log2(){
+//     console.log("hello world 2")
+// }
+// function logWhatsPresent(fn){
+//     fn();
+// }
+
+// logWhatsPresent(log2)
+
+
+
+// const dog={
+//     name: "rocky",
+//     legcount:4,
+//     speaks: "bhow bhow"
+// }
+
+// const cat={
+//     name: "kitty",
+//     legcount: 4,
+//     speaks:"meow meow"
+// }
+
+// console.log("animal "+dog["name"]+""+dog["speaks"])
+// console.log("animal "+cat["name"]+""+cat["speaks"])
+
+
+
+// class Animal{
+//     constructor(name,legcount,speaks){
+//         this.name=name;
+//         this.legcount=legcount;
+//         this.speaks=speaks;
+//     }
+//     speak(){
+//         console.log("hi there "+this.speaks);
+//     }
+// }
+
+// let dog=new Animal("dog",4,"bhow bhow");
+// let cat=new Animal("cat",4,"meow");
+// dog.speak();
+
+
+// const currentDate=new Date();
+// console.log(currentDate.getMonth()+1);
+// console.log(currentDate.getDate());
+// console.log(currentDate.getFullYear());
+
